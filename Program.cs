2a48c77ec618231f4fc130ac9ce8a82d4475f3f8
Sidney Crandall <sdncrandall5@gmail.com>
2021-05-25 10:31:25 -0500

    Phase One complete.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// Set up for the program
+Console.WriteLine("Hello, Can you guess my secret number?");
+// users will be to pick a number
+Console.Write("What number do you choose? ");
+
+// declaring th type 
+string guess = Console.ReadLine();
+
+// return to us what a user guessed
+Console.WriteLine($"You choose {guess}!");
+
+
+
+
